6c5045300a7e470066ca01b3a903ece8ea0fcc66
Maxrimus <joe.p.tursi@gmail.com>
2014-04-13 22:57:04 -0400

    I CAST FIST
    Started working on fist class to get punching working, will then move on
    to buttons and doors.
    
    Have given up on jump for now.

---
+++ Lamia Vittas/Fist.cs
@@ -0,0 +1,39 @@
+﻿/*Joseph Tursi
+ * Date: 4/2/2014
+ * Purpose: A class for the fist
+ * Exceptions:
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+
+namespace Lamia_Vittas
+{
+    class Fist:MovableGamePiece
+    {
+        bool visible;
+
+        public Fist(Rectangle picturesize, Texture2D texture, int dir)
+            : base(picturesize, texture, dir)
+        {
+            visible = false;
+        }
+
+        public void Punch()
+        {
+            visible = true;
+        }
+
+        public void UnPunch()
+        {
+            visible = false;
+        }
+    }
+}
